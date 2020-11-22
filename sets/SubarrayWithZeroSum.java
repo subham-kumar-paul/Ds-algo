264dbbcc57df56ac8d7fcf2ee479b98e788898c4
Subham Kumar Paul <subham.ku.paul@gmail.com>
2020-11-22 14:50:32 +0530

    SubArrayWithZeroSum added

---
+++ sets/SubarrayWithZeroSum.java
@@ -0,0 +1,44 @@
+package sets;
+
+import java.util.*;
+
+/*
+ * Given an array, find if there exists a subarray with sums equal to zero.
+ * n<10^5
+ */
+
+public class SubarrayWithZeroSum {
+
+	public static void main(String[] args) {
+		
+		int a[] = {2, 1, 1, 4, -4} ;
+		boolean found = false ;
+		
+//		for(int i = 0 ; i<a.length; i++) {
+//			int sum = 0 ;
+//			for(int j = i ; j<a.length; j++) {
+//				sum = sum + a[j] ;
+//				if(sum == 0) {
+//					found = true ;
+//					break ;
+//				}					
+//			}
+//			if(found) break ;
+//		}
+		
+		Set<Integer> set = new HashSet<>() ;
+		int sum = 0 ;
+		for(int element : a) {
+			set.add(sum) ;
+			sum = sum + element ;
+			if(set.contains(sum)) {
+				found = true ;
+				break ;
+			}
+		}
+		System.out.println("found " + found) ;
+		
+
+	}
+
+}
