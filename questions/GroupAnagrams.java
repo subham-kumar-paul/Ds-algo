812b6a0b80b78a7c776a1c2419a535c510de2f7c
Subham Kumar Paul <subham.ku.paul@gmail.com>
2020-11-23 23:47:07 +0530

    maps added

---
+++ questions/GroupAnagrams.java
@@ -0,0 +1,44 @@
+package questions;
+
+import java.util.*;
+
+
+public class GroupAnagrams {
+
+	public static void main(String[] args) {
+		Solution solution = new Solution() ;
+		
+		Scanner sc = new Scanner(System.in) ;
+		int n = sc.nextInt() ;
+		String strs[] = new String[n] ; 
+		for(int i = 0 ; i<n ; i++) {
+			strs[i] = sc.next() ;
+		}
+		
+		System.out.println(solution.groupAnagrams(strs)) ;
+
+	}
+
+static class Solution {
+	    public List<List<String>> groupAnagrams(String[] strs) {
+	        
+	        Map<String, List<String>> map = new HashMap<>() ;
+	        for(String s: strs) {
+	            char charArray[] = s.toCharArray() ;
+	            Arrays.sort(charArray) ;
+	            String sorted = new String(charArray) ;
+	            
+	            if(!map.containsKey(sorted)) {
+	                map.put(sorted, new LinkedList<String>())  ;
+	            }
+	            map.get(sorted).add(s) ;
+	            
+	        }
+	        
+	        return new LinkedList<>(map.values()) ;
+	        
+	    }
+	}
+	
+}
+
